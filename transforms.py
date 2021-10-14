ea2e8cfb55b1ba6b9c01cf01a901614a78d52f3d
Rylan Marianchuk <45247316+rylan-marianchuk@users.noreply.github.com>
2021-10-13 21:36:00 -0600

    Add files via upload

--- transforms.py
+++ transforms.py
@@ -99,9 +99,10 @@ class Fourier:
 
 
 class stFourier:
-    def __init__(self, window_size, T=10, fs=500, log=False, normalize=False):
+    def __init__(self, window_size, desired_windows, T=10, fs=500, log=False, normalize=False):
         """
         :param window_size: (int) number of signal elements to use in transform
+        :param desired_windows: (int) amount of fourier windows to take across the signal, solves jump and finds n closest
         :param T: length of signal in seconds
         :param fs: (int) sampling rate of signal
         :param log: (bool) whether to take the logarithm of the resulting power
@@ -113,10 +114,37 @@ class stFourier:
         self.log = log
         self.normalize = normalize
         self.win = window_size
-        self.overlap_frac = 0.5
-        self.jump = math.floor(window_size * self.overlap_frac)
-        self.n_windows = int(math.ceil(self.T*self.fs / self.jump))
-        self.domain = {0: np.array(range(0, T*fs, self.jump)) / fs,
+        # Solve for jump size so that no padding is used
+        # Holding a possible jump size, and number of windows needed given that jump
+        # D[0] - no remainder, D[1] will use 1 zero for padding, D[2] will use 2 zeros for padding
+        D = {
+            0: [],
+            1: [],
+            2: []
+        }
+
+        low_n = int(np.ceil(T * fs / self.win - 1 / self.win))
+        high_n = int(T * fs - self.win)
+        assert (low_n <= desired_windows <= high_n), "Desired number of windows is infeasible for signal"
+        signal_len = int(T * fs)
+        # Populate the dictionary
+        for n in range(low_n, high_n):
+            if (signal_len - 1 - self.win) % (n - 1) == 0:
+                D[0].append((int((signal_len - 1 - self.win) / (n - 1)), n))
+            elif (signal_len - 1 - self.win) % (n - 1) == 1:
+                D[1].append((int((signal_len - 1 - self.win) / (n - 1)), n))
+            elif (signal_len - 1 - self.win) % (n - 1) == 2:
+                D[2].append((int((signal_len - 1 - self.win) / (n - 1)), n))
+
+        # Get the remainder that contains jumps
+        r = 0
+        while len(D[r]) == 0:
+            r += 1
+            if r == 2: break
+
+        self.jump, self.n_windows, diff = sorted([(pair[0], pair[1], abs(pair[1] - desired_windows)) for pair in D[r]], key=lambda x: x[2])[0]
+
+        self.domain = {0: np.array(range(0, T*fs-window_size, self.jump)) / fs,
                        1: np.fft.rfftfreq(self.win, 1/fs)}
         self.domain_shape = (len(self.domain[0]), len(self.domain[1]))
         self.hanning = np.hanning(self.win)
@@ -131,14 +159,16 @@ class stFourier:
                                                     "sample frequency"
         result = np.zeros(shape=(self.domain_shape[0], self.domain_shape[1]))
         signal_pad = np.concatenate((signal, np.zeros(self.win)))
-        for i, L_edge in enumerate((range(0, signal.shape[0], self.jump))):
+        for i, L_edge in enumerate((range(0, signal.shape[0]-self.win, self.jump))):
             dampened = self.hanning * signal_pad[L_edge: L_edge + self.win]
             F = np.fft.rfft(dampened)
             result[i] = np.power(np.abs(F), 2)
             if self.log: result[i] = np.log(result[i])
 
         if self.normalize:
-            return result / np.linalg.norm(result)
+            result -= np.min(result)
+            result /= np.max(result) - np.min(result)
+            return result
         return result
 
     def view(self, signal):
@@ -178,11 +208,14 @@ class PowerSpec:
         :param signal: (float) shape=(n,) to transform
         :return: (float) shape=domain_shape 1D Power Spectrum (magnitude of the FT squared)
         """
+        assert (signal.shape[0] == self.T*self.fs), "The signal is not corresponding to the specified time length and" \
+                                                    "sample frequency"
         if self.log:
             return np.log(np.power(self.FT.magnitude(signal), 2))
         powerSpec = np.power(self.FT.magnitude(signal), 2)
         if self.normalize:
-            return powerSpec / np.linalg.norm(powerSpec)
+            powerSpec -= np.min(powerSpec)
+            powerSpec /= np.max(powerSpec) - np.min(powerSpec)
         return powerSpec
 
     def view(self, signal):
@@ -224,9 +257,13 @@ class Wavelet:
         :param signal: (float) shape=(n,) to transform
         :return: (float) shape=domain_shape 2D image of wavelet coefs
         """
+        assert (signal.shape[0] == self.T*self.fs), "The signal is not corresponding to the specified time length and" \
+                                                    "sample frequency"
         if self.normalize:
             trfm = pywt.cwt(signal, self.widths, self.wavelet)[0]
-            return trfm / np.linalg.norm(trfm)
+            trfm -= np.min(trfm)
+            trfm /= np.max(trfm) - np.min(trfm)
+            return trfm
         return pywt.cwt(signal, self.widths, self.wavelet)[0]
 
     def seeAvailableWavelets(self):
@@ -245,6 +282,56 @@ class Wavelet:
         fig.update_xaxes(title_text="Time (seconds)", type='category')
         fig.show()
 
+
+class binaryImage:
+    def __init__(self, resolution, max, min, T=10, fs=500):
+        """
+        :param resolution: (tuple) shape of desired binary image
+            For now, x axis not scalable
+        :param T: length of signal in seconds
+        :param fs: (int) sampling rate of signal
+        """
+        self.fs = fs
+        self.T = T
+        self.min = min
+        self.max = max
+        self.domain = { 0 : np.linspace(min, max, resolution[0]), # each row a wavelet width is selected and translated across time
+                        1: np.linspace(0, T, T*fs)} # linear time domain sequence
+        self.domain_shape = (len(self.domain[0]), T*fs)
+        return
+
+    def __call__(self, signal):
+        """
+        Transform the signal to its 2D binary image curve
+        :param signal: (float) shape=(n,) to transform
+        :return: (float) shape=domain_shape 2D binary image of the signal
+        """
+        assert (signal.shape[0] == self.T*self.fs), "The signal is not corresponding to the specified time length and" \
+                                                    "sample frequency"
+        result = np.zeros(shape=self.domain_shape)
+        s_min = np.min(signal)
+        s_max = np.max(signal)
+        signal -= s_min
+        signal /= s_max - s_min
+        signal *= self.domain_shape[0] - 1
+        signal = np.rint(signal)
+        for i in range(self.domain_shape[1]):
+            result[int(signal[i]),i] = 1.0
+        return result
+
+
+    def view(self, signal):
+        """
+        Generate plotly graph object to visualize the transform
+        :param signal: (float) shape=(n,) to transform
+        :return: viewable Heatmap plot viewing all wavelet coefficients at a given scale and translation time
+        """
+        trfm = self(signal)
+        fig = go.Figure(data=go.Heatmap(z=trfm, x=self.domain[1], y=self.domain[0]))
+        fig.update_layout(title="Binarized Image")
+        fig.update_yaxes(title_text="Wavelet scale", type='category')
+        fig.update_xaxes(title_text="Time (seconds)", type='category')
+        fig.show()
 
 class GramianAngularField:
     def __init__(self, T=10, fs=500):
